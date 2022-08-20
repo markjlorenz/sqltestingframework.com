57b20c7480a16e59cc1f97e5732674060501b0be
Mark Lorenz <mlorenz@andhealth.com>
2022-08-20 17:27:45 -0400

    histogram checker

---
+++ sample/queries/histogram.sql
@@ -0,0 +1,69 @@
+--  rental_duration
+--  rental_rate
+--  length
+--  replacement_cost
+
+-- \set tbl payment
+-- \set col amount
+\set tbl payment
+\set col amount
+WITH smallest (label, value) AS (
+  SELECT
+     'min'
+     ,MIN(:col)
+  FROM :tbl
+), largest (label, value) AS (
+  SELECT
+    'max'
+    ,MAX(:col)
+  FROM :tbl
+), standard_dev_pop (label, value) AS (
+  SELECT
+    'stddev_pop'
+    ,STDDEV_POP(:col)
+  FROM :tbl
+), median (label, value) AS (
+  SELECT
+     'median'
+    ,PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY :col)
+  FROM :tbl
+), median_deviations AS (
+  SELECT
+    ABS(:col - median.value) AS absolute_deviation
+  FROM :tbl
+  FULL JOIN median ON TRUE
+), mad (label, value) AS ( -- median absoluion deviation
+  SELECT
+     'MAD'
+    ,PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY absolute_deviation)
+  FROM median_deviations
+), histo (buckets, count) AS (
+  SELECT -- Bucket label is where the bucket starts (inclusive)
+     -- (WIDTH_BUCKET(:col, smallest.count, largest.count, 9) - 1) * (largest.count - smallest.count) / 9 + smallest.count AS buckets
+     (WIDTH_BUCKET(:col, mad.value * -6 + median.value, mad.value * 6 + median.value, 12) - 1) * mad.value + (median.value - 6) AS buckets
+    ,COUNT(*) AS count
+  FROM :tbl
+  FULL JOIN smallest ON TRUE
+  FULL JOIN largest  ON TRUE
+  FULL JOIN median   ON TRUE
+  FULL JOIN mad      ON TRUE
+  GROUP BY buckets
+  ORDER BY buckets
+), nulls (none, none) AS (
+  VALUES (NULL::text, NULL::numeric)
+)
+
+SELECT * FROM smallest
+UNION ALL
+SELECT * FROM largest
+UNION ALL
+SELECT * FROM standard_dev_pop
+UNION ALL
+SELECT * FROM median
+UNION ALL
+SELECT * FROM mad
+UNION ALL
+SELECT * FROM nulls
+UNION ALL
+SELECT buckets::text, count FROM histo
+;
